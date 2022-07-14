ebed2f39ddf596df9975fb1f66cde06e00512295
Rohan Magar <72019287+Roahn@users.noreply.github.com>
2022-07-14 18:28:56 +0000

    just made stack folder and push and pop

---
+++ Stack/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+
+
+    public static void main(String args[])
+    {
+        Stack<Integer> stk = new Stack<>();
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        stk.push(4);
+        System.out.println(stk.pop());
+        System.out.println(stk.pop());
+        System.out.println(stk.pop());
+        System.out.println(stk.pop());
+       
+    }
+}
